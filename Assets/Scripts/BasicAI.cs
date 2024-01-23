2dbb8d037634a36126c439eba2528db245d42d55
BaloghSandor <114366334+BaloghSandor@users.noreply.github.com>
2024-01-23 12:10:35 -0500

    Completed basic AI, adding sprint

--- Assets/Scripts/BasicAI.cs
+++ Assets/Scripts/BasicAI.cs
@@ -1,27 +1,16 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicAI : MonoBehaviour
 {
-    public float speed = 23.0F;
-    private Transform target;
-    public GameObject player;
-    
+    public float speed = 1.0f;
     // Start is called before the first frame update
-    void Awake()
-    {
-        // Position the cube at the origin.
-        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-        // Grab cylinder values and place on the target.
-        target = player.Transform;
-        target.transform.position = new Vector3(0.8f, 0.0f, 0.8f);
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         var step =  speed * Time.deltaTime; // calculate distance to move
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("Player").transform.position, step);
     }
 }
