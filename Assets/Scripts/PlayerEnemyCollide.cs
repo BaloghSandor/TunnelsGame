d113252e2aca4fb5e7d9bacf6b09597f17d956ec
baloghs0 <113210115+baloghs0@users.noreply.github.com>
2024-02-11 22:27:20 -0500

    Unfinished prog on GAMEOVER
    Error with PlayerEnemyCollide script reference in GameOver script "due to protection level"

--- Assets/Scripts/PlayerEnemyCollide.cs
+++ Assets/Scripts/PlayerEnemyCollide.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayerEnemyCollide : MonoBehaviour
 {
+    static bool isAlive = true;
     //Detect collisions between the GameObjects with Colliders attached
     void OnCollisionEnter(Collision collision)
     {
-
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
         if (collision.gameObject.tag == "Ennemy")
         {
-            //If the GameObject has the same tag as specified, output this message in the console
-            Debug.Log("Collision detected!");
+            isAlive = false;
         }
     }
 }
