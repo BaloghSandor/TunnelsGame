db4579273658c99ea3de5a121045ecb666b229f7
vallenillaabouassie <vallenillaabouassie@users.noreply.github.com>
2024-04-17 14:51:32 -0400

    Button work yay
    i wanna sound

--- Assets/Scripts/ObjectInteract_test.cs
+++ Assets/Scripts/ObjectInteract_test.cs
@@ -6,12 +6,20 @@ public class ObjectInteract_test : MonoBehaviour
 {
 
     private bool interaction = false;
-
+    private float state = 1.0f;
+    private bool color_switch = false;
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             interaction = true;
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interaction = false;
         }
     }
 
@@ -21,6 +29,18 @@ public class ObjectInteract_test : MonoBehaviour
         {
             var rendering = gameObject.GetComponent<Renderer>();
             rendering.material.SetColor("_Color", Color.green);
+            state *= -1;
+        }
+
+        if(state == 1.0f){
+            color_switch = true;
+        } else {
+            color_switch = false;
+        }
+
+        if(color_switch){
+            var rendering = gameObject.GetComponent<Renderer>();
+            rendering.material.SetColor("_Color", Color.red);
         }
     }
 }
