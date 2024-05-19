86f7f6cb6c8f94f1801f014a7d02cc0cba34a2c4
baloghs0 <113210115+baloghs0@users.noreply.github.com>
2024-05-19 17:43:49 -0400

    Generator component done
    Need to hook it up to lights and health

--- Assets/Scripts/Generator_button_script.cs
+++ Assets/Scripts/Generator_button_script.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectInteract_test : MonoBehaviour
+public class Generator_button_script : MonoBehaviour
 {
 
     private bool interaction = false;
-    private float state = 1.0f;
-    private bool color_switch = false;
+    public Generator_script generator;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -25,22 +25,18 @@ public class ObjectInteract_test : MonoBehaviour
 
     void Update()
     {
-        if (interaction && Input.GetKeyDown(KeyCode.E))
+        if (generator.gen_failure)
+        {
+            var rendering = gameObject.GetComponent<Renderer>();
+            rendering.material.SetColor("_Color", Color.red);
+        }
+
+        if (interaction && Input.GetKeyDown(KeyCode.E) && generator.gen_failure)
         {
             var rendering = gameObject.GetComponent<Renderer>();
             rendering.material.SetColor("_Color", Color.green);
-            state *= -1;
-        }
-
-        if(state == 1.0f){
-            color_switch = true;
-        } else {
-            color_switch = false;
-        }
-
-        if(color_switch){
-            var rendering = gameObject.GetComponent<Renderer>();
-            rendering.material.SetColor("_Color", Color.red);
+            generator.gen_failure = false;
+            generator.launch_new_coroutine = true;
         }
     }
 }
---
+++ Assets/Scripts/Generator_script.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator_script : MonoBehaviour
+{
+    public bool gen_failure = false;
+    public bool launch_new_coroutine = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GeneratorFailTimer(Random.Range(10, 10)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (launch_new_coroutine)
+        {
+            StartCoroutine(GeneratorFailTimer(Random.Range(10, 10)));
+        }
+    }
+
+    IEnumerator GeneratorFailTimer(int time_until_failure)
+    {
+        launch_new_coroutine = false;
+
+        Debug.Log("Waiting Time:" + time_until_failure);
+
+        yield return new WaitForSeconds(time_until_failure);
+
+        gen_failure = true;
+    }
+}
