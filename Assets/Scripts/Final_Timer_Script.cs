9c29710e7f7d4037e57fc7a794e8d99b1fc69d59
baloghs0 <113210115+baloghs0@users.noreply.github.com>
2024-06-20 01:31:19 -0400

    Final Commit for the day

--- Assets/Scripts/Final_Timer_Script.cs
+++ Assets/Scripts/Final_Timer_Script.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Final_Timer_Script : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float FinalTimeLeft;
+    public bool FinalTimerOn = false;
+
+    public Text FinalTimerTxt;
+   
     void Start()
     {
-        
+        FinalTimerOn = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(FinalTimeLeft > 0)
+        {
+            FinalTimeLeft -= Time.deltaTime;
+            updateTimer(FinalTimeLeft);
+        }
+        else
+        {
+            FinalTimeLeft = 0;
+            FinalTimerOn = false;
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        FinalTimerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
